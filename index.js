33c11a85dd45b3943ba9a1336194bc512dbe14a6
Mirza <amirza.techlaunch.io>
2019-10-30 12:43:32 -0400

    person amd email lookup half done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const request = require('request');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 const database = require('./databaseConfig');
 const getPersonData = require('./searchByName');
